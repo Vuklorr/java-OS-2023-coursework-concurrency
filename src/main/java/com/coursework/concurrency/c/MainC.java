4d1471a9b49c59c2c7e6bd22e4ecdc9b3bae7f49
Vuklorr <vova.krypenichka@yandex.ru>
2023-04-17 15:05:53 +0300

    Realise program C

---
+++ src/main/java/com/coursework/concurrency/c/MainC.java
@@ -0,0 +1,62 @@
+package com.coursework.concurrency.c;
+
+import com.coursework.concurrency.c.task.RemoveLongSentences;
+
+import java.util.*;
+
+/**
+ * Курсовая работа.
+ * Долбицын В.С. УИС-311
+ * Вариант 8: "Исключить из текста все предложения длиной более 50 символов"
+ * Программа C
+ */
+public class MainC {
+    public static void main(String[] args) {
+        //проверка на количество аргументов
+        if(args.length < 2) {
+            System.out.println("Введите 2 аргумента:");
+            return;
+        }
+
+        String path = args[0];
+        String newPath = args[1];
+        List<String> fileNames = new ArrayList<>();
+        List<String> newFileNames = new ArrayList<>();
+        //добавление в списки имен файлов
+        for(int i = 1; i < 11; i++) {
+            fileNames.add(path + (i) + ".txt");
+            newFileNames.add(newPath + (i) + ".txt");
+        }
+        Thread[] threads = new Thread[fileNames.size()];
+        Long[] startTimes = new Long[fileNames.size()]; //массив для запоминания начала выполенения метода
+        Long[] endTimes = new Long[fileNames.size()]; //массив для запоминания конца выполенения метода
+
+        long startTime = System.currentTimeMillis(); //время начала работы программы
+
+        //создание и выполнение дочерних потоков
+        for (int i = 0; i < fileNames.size(); i++) {
+            threads[i] = new RemoveLongSentences(i, startTimes, endTimes
+                    , fileNames.get(i), newFileNames.get(i));
+
+            threads[i].setPriority(Thread.MAX_PRIORITY); // установка максимального приоритета
+            threads[i].start();//вызов дочернего потока
+        }
+
+        try {
+            // Соединение всех потоков в один
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        long endTime = System.currentTimeMillis(); //время завершения программы
+        System.out.println("Время выполенения программы:" + (endTime - startTime));
+        for (int i = 0; i < fileNames.size(); i++) {
+            System.out.println("Время начала и конца обработки " + fileNames.get(i) +  " файла:"
+                    + startTimes[i] + " " + endTimes[i] + " "
+                    + (endTimes[i] - startTimes[i]));
+        }
+    }
+
+}
---
+++ src/main/java/com/coursework/concurrency/c/task/RemoveLongSentences.java
@@ -0,0 +1,68 @@
+package com.coursework.concurrency.c.task;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class RemoveLongSentences extends Thread {
+
+    /**
+     * Индекс потока.
+     */
+    private final int index;
+
+    /**
+     * Массив для сохранения начала времени обработки.
+     */
+    private final Long[] startTimes;
+
+    /**
+     * Массив для сохранения конца времени обработки.
+     */
+    private final Long[] endTimes;
+
+    /**
+     * Имя файла, который нужно обработать.
+     */
+    private final String fileName;
+
+    /**
+     * Имя файла, в который нужно записать обработанный файл.
+     */
+    private final String newFileName;
+
+    public RemoveLongSentences(int index, Long[] startTimes, Long[] endTimes, String fileName, String newFileName) {
+        this.index = index;
+        this.startTimes = startTimes;
+        this.endTimes = endTimes;
+        this.fileName = fileName;
+        this.newFileName = newFileName;
+    }
+
+    @Override
+    public void run()  {
+        //System.out.println("Thread: " + index + " priority:" + getPriority());
+        startTimes[index] = System.currentTimeMillis(); // время начала выполнения метода
+
+        File file = new File(fileName);
+        try(Scanner in = new Scanner(new FileInputStream(file));
+            FileWriter fileWriter = new FileWriter(newFileName)) {
+            //выполняется, пока файл не пуст
+            while (in.hasNextLine()) {
+                String sentence = in.nextLine();
+                //найти знак завершения предложения (.?!...)
+                if((sentence.endsWith(".")) || (sentence.endsWith("!")) || sentence.endsWith("?") || sentence.endsWith("...")) {
+                    //если пердложение состоит из 50 или меньше символов - записать его в новый файл
+                    if(sentence.length() <= 50) {
+                        fileWriter.write(sentence + '\n');
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        endTimes[index] = System.currentTimeMillis(); // время конца выполнения метода
+    }
+}
